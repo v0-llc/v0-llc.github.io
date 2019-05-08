b526c383cf9ad07203005cf19c38e8b22ce81157
Teddy Michels <teddy@v0.run>
2019-05-08 18:16:03 -0500

    More work on 3D. Started integration with site.

---
+++ generator.js
@@ -0,0 +1,133 @@
+var dim = 120; // Make divisible by three?
+var matrixX = dim,
+    matrixY = dim;
+
+var noiseDensity = 0.01;
+
+var t = 0;
+var tx = 0,
+    ty = 0,
+    tz = 0;
+
+var simplexX, simplexY, simplexZ;
+var scene, camera, renderer;
+
+var params = {
+    exposure: 1.5,
+    bloomStrength: 1.8,
+    bloomThreshold: 0,
+    bloomRadius: 0.5
+};
+
+var canvasSizeX = window.innerHeight * 3;
+var canvasSizeY = window.innerHeight * 3; 
+
+function initNoise() {
+    // Seed the simplex noise generators
+    simplexX = new SimplexNoise();
+    simplexY = new SimplexNoise();
+    simplexZ = new SimplexNoise();
+}
+
+function initialize() {
+    initNoise();
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, canvasSizeX / canvasSizeY, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(canvasSizeX, canvasSizeY);
+    document.getElementById('circle-nav').appendChild(renderer.domElement);
+}
+
+function initVertices() {
+    for (var x = 0; x < matrixX; x++) {
+        for (var y = 0; y < matrixY; y++) {
+            for (var z = 0; z < 3; z++) {
+                vertArray.push(0);
+                colors.push(1, 1, 1);
+            }
+        }
+    }
+}
+
+function updateVerts() {
+    var positions = mesh.geometry.attributes.position.array;
+    var colors = mesh.geometry.attributes.color;
+
+    for (var x = 0; x < matrixX; x++) {
+        for (var y = 0; y < matrixY; y++) {
+            var posX = simplexX.noise2D(x * noiseDensity + tx, y * noiseDensity + tx);
+            var posY = simplexY.noise2D(x * noiseDensity + ty, y * noiseDensity + ty);
+            var posZ = simplexZ.noise2D(x * noiseDensity + tz, y * noiseDensity + tz);
+
+            positions[(x + y * matrixX) * 3 + 0] = posX;
+            positions[(x + y * matrixY) * 3 + 1] = posY;
+            positions[(x + y * matrixX) * 3 + 2] = posZ;
+
+            colors.setXYZ(
+                x + y * matrixX,
+                simplexX.noise3D(x * noiseDensity, y * noiseDensity, t) * 1,
+                simplexY.noise3D(x * noiseDensity, y * noiseDensity, t) * 0.13,
+                simplexZ.noise3D(x * noiseDensity, y * noiseDensity, t) * 1
+            );
+        }
+    }
+    t += 0.002;
+    tx += 0.0004;
+    ty -= 0.0005;
+    tz += 0.0006;
+
+    mesh.geometry.attributes.position.needsUpdate = true;
+    mesh.geometry.attributes.color.needsUpdate = true;
+}
+
+var vertArray = [];
+var colors = [];
+
+initialize();
+
+initVertices();
+
+var vertices = new Float32Array(vertArray);
+
+var geometry = new THREE.BufferGeometry();
+geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
+geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+geometry.dynamic = true;
+
+var material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    vertexColors: THREE.VertexColors,
+    wireframe: true
+});
+
+var mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+camera.position.z = 3;
+camera.position.x = 1.5;
+
+var renderScene = new THREE.RenderPass(scene, camera);
+
+var bloomPass = new THREE.UnrealBloomPass(new THREE.Vector2(canvasSizeX, canvasSizeY), 1.5, 0.4, 0.85);
+bloomPass.threshold = params.bloomThreshold;
+bloomPass.strength = params.bloomStrength;
+bloomPass.radius = params.bloomRadius;
+
+composer = new THREE.EffectComposer(renderer);
+composer.setSize(canvasSizeX, canvasSizeY);
+composer.addPass(renderScene);
+composer.addPass(bloomPass);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    updateVerts();
+
+
+
+    composer.render(scene, camera);
+}
+
+animate();
