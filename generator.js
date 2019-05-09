3d2ae820a08237731cb779769f843ac2cab61a91
Teddy <teddy@v0.run>
2019-05-09 14:35:37 -0500

    More work on graphics

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-var dim = 120; // Make divisible by three?
+var dim = 90; // Make divisible by three?
 var matrixX = dim,
     matrixY = dim;
 
@@ -20,7 +20,7 @@ var params = {
 };
 
 var canvasSizeX = window.innerHeight * 3;
-var canvasSizeY = window.innerHeight * 3; 
+var canvasSizeY = window.innerHeight * 3;
 
 function initNoise() {
     // Seed the simplex noise generators
@@ -34,7 +34,10 @@ function initialize() {
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, canvasSizeX / canvasSizeY, 0.1, 1000);
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        autoClear: false,
+    });
 
     renderer.setSize(canvasSizeX, canvasSizeY);
     document.getElementById('circle-nav').appendChild(renderer.domElement);
@@ -62,7 +65,7 @@ function updateVerts() {
             var posZ = simplexZ.noise2D(x * noiseDensity + tz, y * noiseDensity + tz);
 
             positions[(x + y * matrixX) * 3 + 0] = posX;
-            positions[(x + y * matrixY) * 3 + 1] = posY;
+            positions[(x + y * matrixX) * 3 + 1] = posY;
             positions[(x + y * matrixX) * 3 + 2] = posZ;
 
             colors.setXYZ(
@@ -99,6 +102,8 @@ geometry.dynamic = true;
 var material = new THREE.LineBasicMaterial({
     color: 0xffffff,
     vertexColors: THREE.VertexColors,
+    linewidth: 5,
+
     wireframe: true
 });
 
@@ -116,9 +121,23 @@ bloomPass.strength = params.bloomStrength;
 bloomPass.radius = params.bloomRadius;
 
 composer = new THREE.EffectComposer(renderer);
+
 composer.setSize(canvasSizeX, canvasSizeY);
+
+
+fxaaPass = new THREE.ShaderPass(THREE.FXAAShader);
+
+var pixelRatio = renderer.getPixelRatio();
+fxaaPass.material.uniforms['resolution'].value.x = 1 / (canvasSizeX * pixelRatio);
+fxaaPass.material.uniforms['resolution'].value.y = 1 / (canvasSizeY * pixelRatio);
+
 composer.addPass(renderScene);
+afterimagePass = new THREE.AfterimagePass();
+afterimagePass.uniforms["damp"].value = 0.75;
+composer.addPass(afterimagePass);
 composer.addPass(bloomPass);
+composer.addPass(fxaaPass); // Not much of a difference?
+
 
 function animate() {
     requestAnimationFrame(animate);
