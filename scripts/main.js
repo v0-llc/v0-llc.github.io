ecb7d7a0af1459ba01546ef983da362e9d538499
v0 LLC <teddy@v0.run>
2025-05-01 12:35:44 -0400

    refactor: global cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,6 @@ $(window).on('load', function() {
   $('#canvas-container canvas').fadeIn();
 })
 
-$('.client-access').click(function() {
-  $('.overlay').fadeIn();
-});
-
-$('.overlay').click(function() {
-  $(this).fadeOut();
-});
-
 $('.credits-button').click(function() {
   $('.credits').fadeToggle();
   $('.logo-central').fadeToggle();
